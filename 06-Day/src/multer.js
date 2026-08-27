11e7c98edc264a7a34de2f533d77f8ebc75dad33
Sanjay Bairagi <mayur92mayur@gmail.com>
2026-08-27 20:39:49 +0530

    add multer in memoryStorage

--- 06-Day/src/multer.js
+++ 06-Day/src/multer.js
@@ -1,15 +1,17 @@
 const multer = require("multer");
-const path = require('path')
+const path = require("path");
 
-const storage = multer.diskStorage({
+const diskStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname , "uploads"));
+    cb(null, path.join(__dirname, "uploads"));
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + file.originalname);
   },
 });
 
-const upload = multer({ storage });
+const memoryStoare = multer.memoryStorage()
+
+const upload = multer({ storage: memoryStoare });
 
 module.exports = upload;
