6e60601954ca7c8d451b4f2112c94b26c179e8ba
Sanjay Bairagi <mayur92mayur@gmail.com>
2026-09-02 23:27:22 +0530

    design user schema

---
+++ 10-Day/src/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const userModel = mongoose.model("Users2", userSchema);
+
+export default userModel;
