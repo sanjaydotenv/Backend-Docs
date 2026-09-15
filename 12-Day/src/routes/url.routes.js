5bfbde0ca241979cacf44b77bd7aee872ac25b0c
Sanjay Bairagi <mayur92mayur@gmail.com>
2026-09-15 22:08:22 +0530

    save url with code

--- 12-Day/src/routes/url.routes.js
+++ 12-Day/src/routes/url.routes.js
@@ -1,9 +1,43 @@
-import express from "express"
+import express from "express";
+import { generateCode } from "../utils/generateCode.js";
+import urlModel from "../models/url.model.js";
 
-const route = express.Router()
+const route = express.Router();
 
+route.post("/", async (req, res) => {
+  const { url } = req.body;
 
+  if (!url) {
+    return res.status(400).json({
+      message: "url is required",
+    });
+  }
 
+  if (
+    url.startsWith("http://") == false &&
+    url.startsWith("https://") == false
+  ) {
+    return res.status(401).json({
+      message: "enter a valid url",
+    });
+  }
 
+  const code = generateCode();
 
-export default route
+  console.log(code);
+
+  const newUrl = await urlModel.create({
+    originalUrl: url,
+    shortCode: code,
+  });
+
+  return res.status(201).json({
+    message: "URL shortened successfully",
+    data: {
+      originalUrl: newUrl.originalUrl,
+      shortCode: newUrl.shortCode,
+    },
+  });
+});
+
+export default route;
---
+++ 12-Day/src/utils/generateCode.js
@@ -0,0 +1,12 @@
+export const generateCode = () => {
+  const mainString =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+  let shortCode = "";
+
+  for (let i = 0; i < 6; i++) {
+    shortCode += mainString.charAt(Math.floor(Math.random() * 62));
+  }
+
+  return shortCode;
+};
