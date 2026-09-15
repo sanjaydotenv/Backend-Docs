d5607b426b6bc251dd445fe1761a7bbb74e92115
Sanjay Bairagi <mayur92mayur@gmail.com>
2026-09-15 20:14:32 +0530

    route set-up

--- 12-Day/src/app/app.js
+++ 12-Day/src/app/app.js
@@ -1,6 +1,10 @@
 import express from "express";
+import urlRoute from "../routes/url.routes.js"
 
 const app = express();
 
+app.use(express.json());
+app.use("/api/url" , urlRoute)
 
-export default app
+
+export default app;
---
+++ 12-Day/src/models/url.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const urlSchema = new mongoose.Schema(
+  {
+    originalUrl: {
+      type: String,
+      required: true,
+    },
+    shortCode: {
+      type: String,
+      required: true,
+    },
+    countClicks: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true },
+);
+
+const urlModel = mongoose.model("urls", urlSchema);
+
+export default urlModel;
