bcba86fd7757bdd925fd102d920c056543479219
Sanjay Bairagi <mayur92mayur@gmail.com>
2026-09-18 18:50:50 +0530

    route set-up

---
+++ 13-Day/src/routes/auth.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const route = Router();
+
+/**
+ * @POST -> http://localhost:3000/api/auth/register
+ */
+
+// route.post("/register" )
+
+
+export default route
--- 13-Day/src/server.js
+++ 13-Day/src/server.js
@@ -1,4 +1,7 @@
 import server from "./app/app.js";
+import connectDB from "./config/db.js";
+
+await connectDB();
 
 server.listen(3000, () => {
   console.log("server is running on port 3000");
