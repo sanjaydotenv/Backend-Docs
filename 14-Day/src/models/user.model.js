3faf2dc1bcc34b9a465d306483e99467ee308fd5
Sanjay Bairagi <mayur92mayur@gmail.com>
2026-09-20 17:28:19 +0530

    Fix user role enum values and enhance validation error handling in registration

--- 14-Day/src/models/user.model.js
+++ 14-Day/src/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String,
     default: "user",
-    enum: ["role", "seller"],
+    enum: ["user", "seller"],
   },
   refreshToken: {
     type: String,
