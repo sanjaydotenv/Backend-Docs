6927efde30ec2e95fdd30db070fd044755ec68f8
Sanjay Bairagi <mayur92mayur@gmail.com>
2026-09-26 12:13:05 +0530

    Add cart model with schema definition for products and user

---
+++ 14-Day/src/models/cart.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema({
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Products",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+        min: 1,
+      },
+      size: {
+        type: String,
+        enum: ["XS", "S", "M", "L", "XL", "XXL"],
+      },
+    },
+  ],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+});
+
+
+const cartModel = mongoose.model("Carts", cartSchema)
+
+export default cartModel
--- 14-Day/src/models/product.model.js
+++ 14-Day/src/models/product.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const productSchema = mongoose.Schema({
+const productSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
